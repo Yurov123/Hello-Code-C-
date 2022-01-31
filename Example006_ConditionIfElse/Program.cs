4abdd9ffdad20010274c68c5a5a3259b6f3f9f74
Konstantin <constantin8271@gmail.com>
2022-01-31 12:45:29 +0300

    Example 006 Condition If-Else

---
+++ Example006_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter yuor name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hello,same Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
