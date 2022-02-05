d7616380e9d9c7c91e24351ed2cea18776c7d236
Konstantin <constantin8271@gmail.com>
2022-02-05 14:34:59 +0300

    example011_method_array

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,25 @@
+﻿// Метод + Массив
+
+int[] array = {14, 42, 33, 4, 65,18, 6, 87, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    } 
+
+    index++;
+}
+
+
+
+
+
+
